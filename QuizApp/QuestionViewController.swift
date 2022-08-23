7ec8f74a3c9b1ffe512a024593c265474004c6f9
JoaoGripp <corguinha@gmail.com>
2022-08-23 13:35:55 -0300

    Refactor and add Cell - Lecture S01E07 finish

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -8,14 +8,49 @@
 import UIKit
 import Foundation
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
-    private var question: String = ""
+    private var question = ""
+    private var options = [String]()
+    private let reuseIdentifier = "Cell"
+    private var selection: ((String) -> Void)? = nil
     
-    convenience init(question: String) {
+    convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
         self.init()
         self.question = question
+        self.options = options
+        self.selection = selection
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headerLabel.text = question
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dequeueCell(in: tableView)
+        
+        cell.textLabel?.text = options[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection?(options[indexPath.row])
+    }
+    
+    private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            return cell
+        }
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
     }
 }
