2f043ca2b21e431b4162786536a9c2dc1de6e7f9
JoaoGripp <corguinha@gmail.com>
2022-08-25 16:23:38 -0300

    Establishing a Presentation Layer in Swift - Lecture S0109 - TDD Finished

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -16,9 +16,9 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     private var question = ""
     private var options = [String]()
     private let reuseIdentifier = "Cell"
-    private var selection: ((String) -> Void)? = nil
+    private var selection: (([String]) -> Void)? = nil
     
-    convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
+    convenience init(question: String, options: [String], selection: @escaping ([String]) -> Void) {
         self.init()
         self.question = question
         self.options = options
@@ -44,7 +44,18 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selection?(options[indexPath.row])
+        selection?(selectedOptions(in: tableView))
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if tableView.allowsMultipleSelection {
+            selection?(selectedOptions(in: tableView))
+        }
+    }
+    
+    private func selectedOptions(in tableView: UITableView) -> [String] {
+        guard let indexPaths = tableView.indexPathsForSelectedRows else { return [] }
+        return indexPaths.map { options[$0.row] }
     }
     
     private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
